f29dc793544d88d28f2136c6196c529ea57ade24
Pankaj kumar Shah <pankaj123.pks@gmail.com>
2022-06-01 01:45:58 +0200

    Create HexToDecimal.js

---
+++ Jealous_Jumpers/HexToDecimal.js
@@ -0,0 +1,26 @@
+/*
+Description:
+Complete the function which converts hex number (given as a string) to a decimal number.
+*/
+
+function hexToDec(hexString){
+    return  parseInt(hexString, 16);
+}
+
+/*
+Sample Test
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+    it("Testing for fixed tests", () => {
+        assert.strictEqual(hexToDec("1"), 1);
+        assert.strictEqual(hexToDec("a"), 10);
+        assert.strictEqual(hexToDec("10"), 16);
+        assert.strictEqual(hexToDec("FF"), 255);
+        assert.strictEqual(hexToDec("-C"), -12);
+    })
+})
+
+*/
