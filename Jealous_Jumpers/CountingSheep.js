31e9a520bdc9a88462ff9f525a23348444f51a97
Pankaj kumar Shah <pankaj123.pks@gmail.com>
2022-06-03 10:42:24 +0200

    Create CountingSheep.js

---
+++ Jealous_Jumpers/CountingSheep.js
@@ -0,0 +1,36 @@
+/*
+Description:
+Consider an array of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+For example,
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+The correct answer would be 17.
+Hint: Don't forget to check for bad values like null/undefined
+*/
+
+function countSheeps(arrayOfSheeps) {
+    return arrayOfSheeps.filter(Boolean).length;
+}
+
+
+/*
+Sample Test
+
+describe("Tests", () => {
+    it("test", () => {
+var array1 = [true,  true,  true,  false,
+              true,  true,  true,  true ,
+              true,  false, true,  false,
+              true,  false, false, true ,
+              true,  true,  true,  true ,
+              false, false, true,  true ];
+              
+Test.assertEquals(countSheeps(array1), 17, "There are 17 sheeps in total")
+    });
+});
+
+*/
