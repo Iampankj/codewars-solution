e9878b44c6de3100ee67827506166da694a13171
Pankaj kumar Shah <pankaj123.pks@gmail.com>
2022-06-03 13:29:30 +0200

    Create ExclamationMarksSeries#1.js

---
+++ Numerous_Nuances/ExclamationMarksSeries#1.js
@@ -0,0 +1,42 @@
+/*
+Description:
+Description:
+Remove a exclamation mark from the end of string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi!!"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+Note
+Please don't post issue about difficulty or duplicate.
+*/
+
+function remove (string) {
+    return string.replace(/!$/,'');
+}
+
+
+/*
+Sample Test
+
+const strictEqual = require("chai").assert.strictEqual;
+
+function doTest(input, expected) {
+	const actual = remove(input);
+	strictEqual(actual, expected, `for string:\n"${input}"\n`);
+}
+
+describe("Tests suite", function () {
+	it("sample tests", function () {
+		doTest("Hi!", "Hi");
+		doTest("Hi!!!", "Hi!!");
+		doTest("!Hi", "!Hi");
+		doTest("!Hi!", "!Hi");
+		doTest("Hi! Hi!", "Hi! Hi");
+		doTest("Hi", "Hi");
+	});
+});
+*/
+
